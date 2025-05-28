cd8cd0a370828cd07a6b8e82d6dcd20357bab6da
Devziur <iamziur@gmail.com>
2025-05-27 22:04:43 -0400

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     // Enable component tagging in development mode
-    base: process.env.VITE_BASE_PATH || "/",
+    base: process.env.VITEG_BASE_PATH || "/venezuela-bakery",
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
